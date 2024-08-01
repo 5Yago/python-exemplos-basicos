4c03e0c94c8fa5a35d38c3ba9248d6ed3073c4b9
Yago <senaiyago44@gmail.com>
2024-08-01 11:39:13 -0300

    1.0

---
+++ soma.py
@@ -0,0 +1,13 @@
+# Variaveis
+n1 = 22
+n2 = 11
+n3 = int
+
+# Cálculo
+ 
+n3 = n1 + n2
+
+# Exibição 
+
+print(n3)
+
---
+++ variaveis.py
@@ -0,0 +1,15 @@
+# Variaveis
+nome = "GIsele"
+idade = "44"
+
+print(nome)
+print(idade)
+
+# 1º método de concatenação 
+
+print("Meu nome é: " + nome + "e tenho" +str(idade) + " anos.")
+
+print ("O meu nome é: {} e tenho {} anos.\n" .format(nome, idade))
+
+print (f"O meu nome é: {nome} e tenho {idade} anos")
+
